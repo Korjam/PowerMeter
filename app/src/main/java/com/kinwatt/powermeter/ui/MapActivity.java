b9ec005ad814f3126935b4185ac1a90ff8d9a048
Francisco José Torralvo <frascoj93@hotmail.com>
2018-04-07 16:44:46 +0200

    Code clean in MapActivity

--- app/src/main/java/com/kinwatt/powermeter/ui/MapActivity.java
+++ app/src/main/java/com/kinwatt/powermeter/ui/MapActivity.java
@@ -10,21 +10,18 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
 import com.kinwatt.powermeter.R;
 import com.kinwatt.powermeter.data.Position;
 import com.kinwatt.powermeter.ui.widget.ChronometerView;
 import com.kinwatt.powermeter.ui.widget.NumberView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-    private List<Position> positions = new ArrayList<>();
 
     private MapController controller;
 
@@ -80,6 +77,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         controller = new MapController(this);
     }
 
+    private PolylineOptions options;
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
@@ -88,41 +86,23 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         LatLng malaga = new LatLng(36.7161622, -4.4233658);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(malaga));
         mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
-        /*
-        Position firstPosition = record.getPositions().get(0);
-        LatLng initial = new LatLng(firstPosition.getLatitude(), firstPosition.getLongitude());
 
-        Position lastPosition = record.getLastPosition();
-        LatLng end = new LatLng(lastPosition.getLatitude(), lastPosition.getLongitude());
-
-        mMap.addMarker(new MarkerOptions().position(initial).title("Start"));
-        mMap.addMarker(new MarkerOptions().position(end).title("End"));
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(initial));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
-
-        PolylineOptions options = new PolylineOptions().width(15).color(Color.BLUE).geodesic(true);
-        for (Position position: record.getPositions()) {
-            options.add(new LatLng(position.getLatitude(), position.getLongitude()));
-        }
-
-        mMap.addPolyline(options);
-        */
+        options = new PolylineOptions()
+                .width(15)
+                .color(Color.BLUE)
+                .geodesic(true)
+                .jointType(JointType.ROUND)
+                .startCap(new RoundCap())
+                .endCap(new RoundCap());
     }
 
     public void updateMap(Position position) {
-        // TODO: Mock of a Google Map update.
-        positions.add(position);
         if (mMap != null) {
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(position.getLatitude(), position.getLongitude())));
+
+            options.add(new LatLng(position.getLatitude(), position.getLongitude()));
+
             mMap.clear();
-
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(position.getLatitude(), position.getLongitude())));
-
-            PolylineOptions options = new PolylineOptions().width(15).color(Color.BLUE).geodesic(true);
-            for (Position p : positions) {
-                options.add(new LatLng(p.getLatitude(), p.getLongitude()));
-            }
-
             mMap.addPolyline(options);
         }
     }
@@ -136,9 +116,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
     public void clearMap() {
-        positions.clear();
         if (mMap != null) {
             mMap.clear();
+            options.getPoints().clear();
         }
     }
 }

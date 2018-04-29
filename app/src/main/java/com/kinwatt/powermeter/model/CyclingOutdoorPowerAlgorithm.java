cd7523296fbb1ad9e76c2e22f82aba634f3d0cd6
Gutisiano <gutisiano@gmail.com>
2018-04-29 12:16:23 +0200

    Use CdA value from user's height and weight

--- app/src/main/java/com/kinwatt/powermeter/model/CyclingOutdoorPowerAlgorithm.java
+++ app/src/main/java/com/kinwatt/powermeter/model/CyclingOutdoorPowerAlgorithm.java
@@ -10,28 +10,32 @@ public class CyclingOutdoorPowerAlgorithm extends PowerAlgorithm {
     //For now I'm setting the user's parameters here until we define the class User.
     private static final float gForce = 9.80665f;
     private static final float cRolling = 0.004f;
-    private static final float CdA = 0.32f;
+    private static float CdA;
     private static final float rho = 1.226f;
-    private static final float drag = CdA * rho / 2;
+
 
     private final float totalMass;
+
 
     public CyclingOutdoorPowerAlgorithm(User user) {
         super(user);
         if (user != null) {
             totalMass = user.getWeight() + user.getBikes().get(0).getWeight();
+            CdA = (float) (0.8 * (0.0285 * Math.pow(user.getHeight()/100,0.725) * Math.pow(user.getWeight(),0.425) + 0.17));
         }
         else {
             totalMass = 80;
+            CdA=0.36f;
         }
     }
+
+    private static final float drag = CdA * rho / 2;
 
     @Override
     public float calculatePower(Location pos1, Location pos2) {
         //Calculating increments
         // It should be minus the Position altitude x meters before (25m-50m is fine). A less demanding approximation is to get altitude from the Position instance x/p1.getAltitude() seconds before.
         double hDiff = pos2.getAltitude() - pos1.getAltitude();
-
         double grade = hDiff / pos1.distanceTo(pos2);
         return calculatePower(pos1, pos2, grade);
     }

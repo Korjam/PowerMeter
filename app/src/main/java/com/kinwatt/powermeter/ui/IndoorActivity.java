024f48c928694f59e1348402dfba4038f85a70f9
Gutisiano <gutisiano@gmail.com>
2018-05-01 19:31:55 +0200

    Creating an activity for indoor training (copy from MapActivity,...)

---
+++ app/src/main/java/com/kinwatt/powermeter/ui/IndoorActivity.java
@@ -0,0 +1,143 @@
+package com.kinwatt.powermeter.ui;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.JointType;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
+import com.kinwatt.powermeter.R;
+import com.kinwatt.powermeter.data.Position;
+import com.kinwatt.powermeter.ui.widget.ChronometerView;
+import com.kinwatt.powermeter.ui.widget.NumberView;
+
+public class IndoorActivity extends ActivityBase implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private PolylineOptions options;
+    private Polyline line;
+
+    private IndoorController controller;
+
+    private Button buttonStart, buttonStop;
+
+    private NumberView power;
+    private NumberView speed;
+    private ChronometerView duration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indoor);
+
+        buttonStart = findViewById(R.id.button_start);
+        buttonStop = findViewById(R.id.button_stop);
+
+        buttonStop.setEnabled(false);
+
+        duration = findViewById(R.id.duration);
+        speed = findViewById(R.id.speed);
+        power = findViewById(R.id.power);
+
+        speed.setUnits("km/h");
+        power.setUnits("W");
+
+        buttonStart.setOnClickListener(v -> {
+            buttonStart.setEnabled(false);
+
+            duration.restart();
+            controller.start();
+
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            buttonStop.setEnabled(true);
+        });
+
+        buttonStop.setOnClickListener(v -> {
+            buttonStop.setEnabled(false);
+
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+            duration.stop();
+            controller.stop();
+
+            buttonStart.setEnabled(true);
+        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        controller = new IndoorController(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        controller.stop();
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        LatLng malaga = new LatLng(36.7161622, -4.4233658);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(malaga));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+
+        options = new PolylineOptions()
+                .width(15)
+                .color(getResources().getColor(R.color.colorPrimary))
+                .geodesic(true)
+                .jointType(JointType.ROUND)
+                .startCap(new RoundCap())
+                .endCap(new RoundCap());
+    }
+
+    public void updateMap(Position position) {
+        if (mMap != null) {
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(position.getLatitude(), position.getLongitude())));
+
+            LatLng point = new LatLng(position.getLatitude(), position.getLongitude());
+
+            if (!options.getPoints().contains(point)) {
+                options.add(point);
+            }
+
+            Polyline newLine = mMap.addPolyline(options);
+            if (line != null) {
+                line.remove();
+            }
+            line = newLine;
+        }
+    }
+
+    public void stopTimer() {
+        duration.stop();
+        buttonStop.setEnabled(false);
+        buttonStart.setEnabled(true);
+    }
+
+    public void setSpeed(float speed) {
+        this.speed.setValue(speed);
+    }
+
+    public void setPower(float power) {
+        this.power.setValue(power);
+    }
+
+    public void clearMap() {
+        if (mMap != null) {
+            mMap.clear();
+            options.getPoints().clear();
+        }
+    }
+}
---
+++ app/src/main/java/com/kinwatt/powermeter/ui/IndoorController.java
@@ -0,0 +1,217 @@
+package com.kinwatt.powermeter.ui;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.widget.Toast;
+
+import com.kinwatt.powermeter.common.LocationUtils;
+import com.kinwatt.powermeter.common.MathUtils;
+import com.kinwatt.powermeter.data.Position;
+import com.kinwatt.powermeter.data.Record;
+import com.kinwatt.powermeter.data.SensorData;
+import com.kinwatt.powermeter.data.User;
+import com.kinwatt.powermeter.data.mappers.UserMapper;
+import com.kinwatt.powermeter.data.provider.SensorProvider;
+import com.kinwatt.powermeter.model.CyclingIndoorPowerAlgorithm;
+import com.kinwatt.powermeter.model.CyclingOutdoorPowerAlgorithm;
+import com.kinwatt.powermeter.sensor.SpeedListener;
+import com.kinwatt.powermeter.sensor.bluetooth.SpeedAndCadenceClient;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class IndoorController implements SpeedListener {
+
+
+    private IndoorActivity activity;
+
+    private CyclingIndoorPowerAlgorithm indoor;
+    private CyclingOutdoorPowerAlgorithm outdoor;
+
+    private SpeedAndCadenceClient speedProvider;
+    private float speed;
+
+    private Timer timer;
+    private TimerTask timerTask;
+
+    private Record track;
+    private List<Float> distances;
+
+    private boolean running;
+
+    public IndoorController(IndoorActivity activity) {
+        this.activity = activity;
+        running = false;
+
+        indoor = new CyclingIndoorPowerAlgorithm(null);
+        outdoor = new CyclingOutdoorPowerAlgorithm(null);
+
+        BluetoothManager bluetoothManager = (BluetoothManager) activity.getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        if (!bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.enable();
+        }
+
+        SensorProvider provider = SensorProvider.getProvider(activity);
+        SensorData data = provider.findSensor(SpeedAndCadenceClient.SERVICE_UUID);
+
+        if (data != null) {
+            BluetoothDevice device = bluetoothAdapter.getRemoteDevice(data.getAddress());
+
+            speedProvider = new SpeedAndCadenceClient(activity, device);
+            speedProvider.addListener(this);
+        }
+
+        track = readTrack("gibralfaro.txt");
+        distances = getDistances(track);
+    }
+
+    public void start() {
+        if (!running) {
+            activity.clearMap();
+            activity.updateMap(track.getPositions().get(0));
+            speedProvider.connect();
+            createTimer();
+            timer.schedule(timerTask, 0, 1000);
+            running = true;
+        }
+    }
+
+    public void stop() {
+        if (running) {
+            speedProvider.close();
+            timer.cancel();
+            running = false;
+        }
+    }
+
+    @Override
+    public void onSpeedChanged(float rpm) {
+        // Speed in m/s
+        this.speed = rpm * 2.099f / 60;
+    }
+
+    private void createTimer() {
+        timer = new Timer();
+        timerTask = new TimerTask() {
+            private float totalDistance = distances.get(distances.size() - 1);
+            private float distance, speedOutdoor, lastSpeed;
+
+            private static final float MULTIPLIER = 4;
+
+            @Override
+            public void run() {
+                int index = findIndex(distance, distances);
+
+                Position p1 = track.getPositions().get(index);
+                Position p2 = track.getPositions().get(index + 1);
+
+                float speed = IndoorController.this.speed;
+                p1.setSpeed(lastSpeed);
+                p2.setSpeed(speed);
+                float power = indoor.calculatePower(Position.convert(p1), Position.convert(p2));
+
+                speedOutdoor = outdoor.calculateSpeed(power, speedOutdoor, getGrade(p1, p2));
+
+                distance += speedOutdoor * MULTIPLIER;
+                distance = Math.min(distance, totalDistance);
+                lastSpeed = speed;
+
+                index = Math.min(findIndex(distance, distances), distances.size() - 2);
+                p1 = track.getPositions().get(index);
+                p2 = track.getPositions().get(index + 1);
+
+                long time = MathUtils.interpolate(
+                        distances.get(index),     p1.getTimestamp(),
+                        distances.get(index + 1), p2.getTimestamp(), distance);
+
+                Position current = LocationUtils.interpolate(p1, p2, time);
+
+                activity.runOnUiThread(() ->  {
+                    activity.setSpeed(speedOutdoor * 3.6f);
+                    activity.setPower(power);
+                    activity.updateMap(current);
+                    if (distance >= totalDistance) {
+                        activity.stopTimer();
+                    }
+                });
+
+                if (distance >= totalDistance) {
+                    stop();
+                }
+            }
+        };
+    }
+
+    /***
+     * On a sorted list, returns the index of the value
+     * which is immediately below to <code>value</code>.
+     * @param value
+     * @param values
+     * @return
+     */
+    private static int findIndex(float value, List<Float> values) {
+        for (int i = 0; i < values.size() - 1; i++) {
+            if (values.get(i) <= value && values.get(i + 1) > value) {
+                return i;
+            }
+        }
+        return value >= values.get(values.size() - 1) ? values.size() : -1;
+    }
+
+    private static double getGrade(Position p1, Position p2){
+        double hDiff = p2.getAltitude() - p1.getAltitude();
+        return hDiff / p2.getDistance(p1);
+    }
+
+    private static List<Float> getDistances(Record track) {
+        return getDistances(track.getPositions());
+    }
+
+    private static List<Float> getDistances(List<Position> positions) {
+        List<Float> res = new ArrayList<>();
+
+        res.add(0f);
+
+        float distance = 0;
+
+        for (int i = 1; i < positions.size(); i++) {
+            Position p1 = positions.get(i - 1);
+            Position p2 = positions.get(i);
+            distance += p1.getDistance(p2);
+            res.add(distance);
+        }
+
+        return res;
+    }
+
+    private Record readTrack(String filePath) {
+        Record track = new Record();
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(activity.getAssets().open(filePath)));
+
+            String strLine;
+            long time = 0;
+            while ((strLine = br.readLine()) != null)   {
+                String [] values = strLine.split(";");
+
+                Position p = new Position(Float.parseFloat(values[1]), Float.parseFloat(values[2]), Float.parseFloat(values[0]));
+                p.setTimestamp(time);
+                track.getPositions().add(p);
+                time += 1000;
+            }
+            br.close();
+        } catch (IOException e) {
+            Toast.makeText(activity, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return track;
+    }
+}

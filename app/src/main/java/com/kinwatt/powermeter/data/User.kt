cbd09d2ab6925dfc75a01952c135b11d8382ee8b
Francisco José Torralvo <frascoj93@hotmail.com>
2019-01-07 12:08:56 +0100

    User to kotlin

---
+++ app/src/main/java/com/kinwatt/powermeter/data/User.kt
@@ -0,0 +1,8 @@
+package com.kinwatt.powermeter.data
+
+import java.util.ArrayList
+
+class User(var name: String = "", var age: Int = 0, var weight: Float = 0f, var height: Int = 0) {
+
+    val bikes: List<Bike> = ArrayList()
+}
